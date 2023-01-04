e7e0e939a44e00f1f6047fef2c52e7caa921d744
phadalgekar <hadalgekarprathamesh@gmail.com>
2023-01-04 15:12:21 +0530

    adding welcome to user registration

---
+++ src/main/java/com/datastructure/UserRegistration/App.java
@@ -0,0 +1,10 @@
+package com.datastructure.UserRegistration;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to User Registration" );
+    }
+}
