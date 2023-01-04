76ef9db73d8531a68de2dfd7ef2fff3413fa736e
phadalgekar <hadalgekarprathamesh@gmail.com>
2023-01-04 16:09:02 +0530

    adding program of regex pattern to enter valid email from user

--- src/main/java/com/datastructure/UserRegistration/UserRegistration.java
+++ src/main/java/com/datastructure/UserRegistration/UserRegistration.java
@@ -11,21 +11,33 @@ public class UserRegistration {
         String firstName = scan.nextLine();
         System.out.println("Enter the last Name :");
         String lastName = scan.nextLine();
+        System.out.println("Enter The Mail : ");
+        String mail = scan.nextLine();
 
         if (Pattern.matches("^[A-Z][a-z]{2,}", firstName)) {
-            System.out.println("First name is valid");
+            System.out.println("True");
         } else {
-            System.out.println("Entered first name is invalid");
+            System.out.println("False");
+        }
+
+        // Method To Check Last Name With Regex Start With Cap And 3 Min. Character
+        if (Pattern.matches("^[A-Z][a-z]{2,}", lastName)) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
+
+        // Method to check Valid Mail with Regex
+        if (Pattern.matches("^[a-zA-Z0-9]+[@][a-zA-Z]{3,15}[.][a-zA-Z]{2,5}", mail)) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
         }
         scan.close();
-	// Method To Check Last Name With Regex Start With Cap And 3 Min. Character
-    if (Pattern.matches("^[A-Z][a-z]{2,}", lastName)) {
-        System.out.println("Last name is valid");
-    } else {
-        System.out.println("Entered last name is invalid");
-    }
 	}
 }
+
+	
 
 	
 
